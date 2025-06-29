a00b4f7a7e05551ef48eb4f787b5681e54c07b53
ngiovana <giovana_niehues@hotmail.com>
2025-06-29 10:19:27 -0300

    chore: comentando método não utilizado

--- FinanceBackend/src/main/java/com/example/FinanceBackend/repository/UserRepository.java
+++ FinanceBackend/src/main/java/com/example/FinanceBackend/repository/UserRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Boolean existsByUsername(String username);
+//    Boolean existsByUsername(String username);
     Optional<User> findByEmail(String email);
     Boolean existsByEmail(String email);
 }
